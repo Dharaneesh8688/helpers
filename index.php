43257d7f0aa70eaa544f02cc73c3ddfadb0a3a68
Dharaneesh8688 <dharaneesh8688@gmail.com>
2024-01-24 14:11:42 +0530

    update

--- index.php
+++ index.php
@@ -3,12 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+  
     <title>HELPERS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-   
+    <style>
+      
+     
+      body{
+        background-color:gray;
+        color:white;
+
+      }
+  table,th,tr,td {
+    border:2px solid white;
+    padding:2px;
+  
+  }
+</style>
 </head>
+
 <body>
+
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">HELPERS</a>
@@ -16,19 +32,19 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 " style="margin-left:80%";>
+      <ul class="navbar-nav me-auto mb-2  mb-lg-0 " style="margin-left:70%";>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="form.php">register</a>
+          <a class="nav-link" href="form.php">Register</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            more
+            More
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="delet.php">delet doner</a></li>
+            <li><a class="dropdown-item" href="delet.php">Delet doner</a></li>
             <li><a class="dropdown-item" href="#">about</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">search</a></li>
@@ -40,20 +56,18 @@
     </div>
   </div>
 </nav>
-
-<form method="post" action="index.php">
+<div class="container">
+  <div class="row ">
+    <div class="col-md-6 justify-content-center ">
+    <form method="post" action="index.php" style="margin-top: 100px;text-align:justify;">
   <label for="city">City:</label>
-  <input type="text" id="city" name="city">
+  <input type="text" id="city" name="city" style="text-transform: uppercase;"><br><br>
   <label for="blood">Blood:</label>
-  <input type="text" id="blood" name="blood">
+  <input type="text" id="blood" name="blood" style="text-transform: uppercase;"><br><br>
   <input type="submit" value="Search">
 </form>
-
-
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
-</html>
+  </div>
+<div class="col-md-6 text-center mt-5 ">
 <?php
 include 'connection.php';
 
@@ -68,25 +82,44 @@ $result = mysqli_query($conn, $sql);
 if (!$result) {
   die("Query failed: " . mysqli_error($conn));
 }
-
+?>
+<?php
 // Data display
-if (mysqli_num_rows($result) > 0) {
-  echo "<table>";
-  echo "<tr><th>NAME</th><th>DOB</th><th>BLOOD</th><th>MOBILE</th></tr>";
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<td>" . $row["NAME"] . "</td>";
-    echo "<td>" . $row["DOB"] . "</td>";
-    echo "<td>" . $row["BLOOD"] . "</td>";
-    echo "<td>" . $row["mobile_no"] . "</td>";
-    echo "</tr>";
-  }
-  echo "</table>";
+if (mysqli_num_rows($result) > 0) {?>
+  <table class="mx-3">
+  <tr><th>NAME</th><th>AVAILABLE</th><th>BLOOD</th><th>MOBILE</th></tr>
+  <?php while($row = mysqli_fetch_assoc($result)) {
+    ?>
+    <tr>
+    <td><?php echo $row["NAME"]; ?></td>
+    <td><?php echo $row["AVAILABLE"]; ?></td>
+    <td><?php echo $row["BLOOD"]; ?></td>
+    <td><?php echo $row["mobile_no"]; ?></td>
+    </tr>
+    
+    <?php
+  }?>
+  </table>
+  <?php
+
 } else {
   echo "No results found";
 }
+?>
+</div>
+</div>
+</div>
 
+
+<?php
 // Database connection close
 mysqli_close($conn);
 }
 ?>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</html>
+
+
+
